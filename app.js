6804aa8e2c3f33a739d04ab89cdf2483e6c15dd5
Clément LEFIEF <clement@weecoop.org>
2022-03-15 18:51:43 +0100

    add cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/student', indexRouter);
 
